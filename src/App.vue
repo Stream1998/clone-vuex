cef2e0c77bcd4de4e5d86e18ebb4cbb251c4e758
Stream1998 <2306120413@qq.com>
2023-12-13 15:33:52 +0800

    init vuex

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <p>num: {{ this.$store.state.num }}</p>
   </div>
 </template>
 
@@ -12,11 +13,6 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 24px;
 }
 </style>
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+	state: {
+		num: 0,
+	},
+	getters: {},
+	mutations: {},
+	actions: {},
+});
+
+export default store;
